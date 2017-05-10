0debcb2b17cff3f8428dc5c98169f65515b54144
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-09 20:17:23 -0700

    Create initial files and folder.

---
+++ js/scrips.js
@@ -0,0 +1,9 @@
+/// Business Logic
+function BankAccount(name, initialDeposit) {
+  this.name   = name;
+  this.amount = initialDeposit;
+}
+
+/// User Interface Logic ///
+
+$(document).ready(function() {
